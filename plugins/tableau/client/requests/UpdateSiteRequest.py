99fd58502cd53a5ff744a7dc48b93b0784095a71
Elliott Stam <elliott.stam@gmail.com>
2019-08-08 15:27:07 -0700

    Updated class: UpdateSiteRequest Updated parameters and fixed bugs for updating sites

--- plugins/tableau/client/requests/UpdateSiteRequest.py
+++ plugins/tableau/client/requests/UpdateSiteRequest.py
@@ -9,6 +9,8 @@ class UpdateSiteRequest(BaseRequest):
     :type ts_connection:                    class
     :param site_name:                       (Optional) The new name of the site.
     :type site_name:                        string
+    :param content_url:                     (Optional) The new content url for the site (typically same as site name).
+    :type content_url:                      string
     :param content_url:                     (Optional) The new site URL. This value can contain only characters that
                                             are valid in a URL.
     :type content_url:                      string
@@ -33,9 +35,9 @@ class UpdateSiteRequest(BaseRequest):
                                             If you set a quota and the site exceeds it, publishers will be prevented
                                             from uploading new content until the site is under the limit again.
     :type storage_quota:                    string
-    :param disable_subscriptions_flag:      Boolean flag; True if users can subscribe to workbooks or views on the
+    :param disable_subscriptions:           Boolean flag; True if users can subscribe to workbooks or views on the
                                             site, False otherwise.
-    :type disable_subscriptions_flag:       boolean
+    :type disable_subscriptions:            boolean
     :param flows_enabled_flag:              Boolean flag; True if flows are enabled on the site, False otherwise.
     :type flows_enabled_flag:               boolean
     :param guest_access_enabled_flag:       Boolean flag; True if guest access is enabled on the site, False otherwise.
@@ -62,7 +64,7 @@ class UpdateSiteRequest(BaseRequest):
                  user_quota=None,
                  state='Active',
                  storage_quota=None,
-                 disable_subscriptions_flag=None,
+                 disable_subscriptions=None,
                  flows_enabled_flag=None,
                  guest_access_enabled_flag=False,
                  cache_warmup_enabled_flag=False,
@@ -79,7 +81,7 @@ class UpdateSiteRequest(BaseRequest):
         self._user_quota = user_quota
         self._state = state
         self._storage_quota = storage_quota
-        self._disable_subscriptions_flag = disable_subscriptions_flag
+        self._disable_subscriptions = disable_subscriptions
         self._flows_enabled_flag = flows_enabled_flag
         self._guest_access_enabled_flag = guest_access_enabled_flag
         self._cache_warmup_enabled_flag = cache_warmup_enabled_flag
@@ -115,14 +117,21 @@ class UpdateSiteRequest(BaseRequest):
             self._admin_mode,
             self._state,
             self._storage_quota,
-            'true' if self._disable_subscriptions_flag is True else 'false' if self._disable_subscriptions_flag is False else None,
-            'true' if self._flows_enabled_flag is True else 'false' if self._flows_enabled_flag is False else None,
-            'true' if self._guest_access_enabled_flag is True else 'false' if self._guest_access_enabled_flag is False else None,
-            'true' if self._cache_warmup_enabled_flag is True else 'false' if self._cache_warmup_enabled_flag is False else None,
-            'true' if self._commenting_enabled_flag is True else 'false' if self._commenting_enabled_flag is False else None,
-            'true' if self._revision_history_enabled is True else 'false' if self._revision_history_enabled is False else None,
+            'true' if self._disable_subscriptions is True
+            else 'false' if self._disable_subscriptions is False else None,
+            'true' if self._flows_enabled_flag is True
+            else 'false' if self._flows_enabled_flag is False else None,
+            'true' if self._guest_access_enabled_flag is True
+            else 'false' if self._guest_access_enabled_flag is False else None,
+            'true' if self._cache_warmup_enabled_flag is True
+            else 'false' if self._cache_warmup_enabled_flag is False else None,
+            'true' if self._commenting_enabled_flag is True
+            else 'false' if self._commenting_enabled_flag is False else None,
+            'true' if self._revision_history_enabled is True
+            else 'false' if self._revision_history_enabled is False else None,
             self._revision_limit,
-            'true' if self._subscribe_others_enabled_flag is True else 'false' if self._subscribe_others_enabled_flag is False else None
+            'true' if self._subscribe_others_enabled_flag is True
+            else 'false' if self._subscribe_others_enabled_flag is False else None
         ]
 
     @property
