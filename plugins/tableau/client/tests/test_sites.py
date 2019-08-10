faddf68f7bf60f42ae96fa6f6a7c6cd6c911a52a
Elliott Stam <elliott.stam@gmail.com>
2019-08-09 23:00:54 -0700

    Updated test: test_sites.py Added tests: test_update_site_state, test_update_site_storage_quota, test_disable_subscriptions_flag

--- plugins/tableau/client/tests/test_sites.py
+++ plugins/tableau/client/tests/test_sites.py
@@ -4,6 +4,8 @@ from tableau.client.config.config import tableau_server_config
 TEST_SITE_NAME = 'estam_api_test'
 TEST_SITE_CONTENT_URL = 'estam_api_test'
 UPDATED_CONTENT_URL = 'estam_api_test_renamed'
+DEFAULT_ADMIN_MODE = 'ContentAndUsers'
+DEFAULT_STATE = 'Active'
 
 
 def sign_in():
@@ -43,11 +45,42 @@ def update_site_name(conn, new_content_url):
     return conn, response
 
 
+def update_site_admin_mode(conn, new_admin_mode):
+    site_json = conn.query_site().json()
+    site_id = site_json['site']['id']
+    response_a = conn.update_site(site_id=site_id, admin_mode=new_admin_mode)
+    response_b = conn.update_site(site_id=site_id, admin_mode=DEFAULT_ADMIN_MODE)
+    return conn, response_a, response_b
+
+
 def update_site_user_quota(conn, new_user_quota):
     site_json = conn.query_site().json()
     site_id = site_json['site']['id']
     response = conn.update_site(site_id=site_id, user_quota=new_user_quota)
     return conn, response
+
+
+def update_site_state(conn, new_state):
+    site_json = conn.query_site().json()
+    site_id = site_json['site']['id']
+    response_a = conn.update_site(site_id=site_id, state=new_state)
+    response_b = conn.update_site(site_id=site_id, state=DEFAULT_STATE)
+    return conn, response_a, response_b
+
+
+def update_site_storage_quota(conn, new_storage_quota):
+    site_json = conn.query_site().json()
+    site_id = site_json['site']['id']
+    response = conn.update_site(site_id=site_id, storage_quota=new_storage_quota)
+    return conn, response
+
+
+def update_site_disable_subscriptions_flag(conn):
+    site_json = conn.query_site().json()
+    site_id = site_json['site']['id']
+    response_a = conn.update_site(site_id=site_id, disable_subscriptions_flag=True)
+    response_b = conn.update_site(site_id=site_id, disable_subscriptions_flag=False)
+    return conn, response_a, response_b
 
 
 def delete_site(conn):
@@ -93,11 +126,46 @@ def test_update_site_name():
     conn.sign_out()
 
 
+def test_update_site_admin_mode():
+    conn = sign_in()
+    conn.switch_site(UPDATED_CONTENT_URL)
+    conn, update_site_response_a, update_site_response_b = update_site_admin_mode(conn, 'ContentOnly')
+    assert update_site_response_a.status_code == 200
+    assert update_site_response_b.status_code == 200
+    conn.sign_out()
+
+
 def test_update_site_user_quota():
     conn = sign_in()
     conn.switch_site(UPDATED_CONTENT_URL)
-    conn, update_site_response = update_site_user_quota(conn, '15')
+    conn, update_site_response = update_site_user_quota(conn, 15)
     assert update_site_response.status_code == 200
+    conn.sign_out()
+
+
+def test_update_site_state():
+    conn = sign_in()
+    conn.switch_site(UPDATED_CONTENT_URL)
+    conn, update_site_response_a, update_site_response_b = update_site_state(conn, 'Suspended')
+    assert update_site_response_a.status_code == 200
+    assert update_site_response_b.status_code == 200
+    conn.sign_out()
+
+
+def test_update_site_storage_quota():
+    conn = sign_in()
+    conn.switch_site(UPDATED_CONTENT_URL)
+    conn, update_site_response = update_site_storage_quota(conn, 1024)
+    assert update_site_response.status_code == 200
+    conn.sign_out()
+
+
+def test_update_site_disable_subscriptions_flag():
+    conn = sign_in()
+    conn.switch_site(UPDATED_CONTENT_URL)
+    conn, update_site_response_a, update_site_response_b = update_site_disable_subscriptions_flag(conn)
+    assert update_site_response_a.status_code == 200
+    assert update_site_response_b.status_code == 200
     conn.sign_out()
 
 
