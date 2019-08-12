5f8607240693d49d2fa02de565cd855c5f157124
Elliott Stam <elliott.stam@gmail.com>
2019-08-12 14:27:25 -0700

    Updated __init__.py files to import various functions and classes

---
+++ plugins/tableau/client/__init__.py
@@ -0,0 +1,6 @@
+from .tableau_server_connection import TableauServerConnection
+from ..client import config
+from ..client import decorators
+from ..client import endpoints
+from ..client import exceptions
+from ..client import requests
